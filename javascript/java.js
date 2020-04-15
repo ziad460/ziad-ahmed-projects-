86ff40800ce7325c1ccfb63561bcdd0343de2794
ziad460 <ziad5827@gmail.com>
2020-04-15 23:12:18 +0200

    first commit

---
+++ javascript/java.js
@@ -0,0 +1,40 @@
+// var home = document.getElementById("home");
+// var shop = document.getElementById("shop");
+// var sale = document.getElementById("sale");
+// var custcares = document.getElementById("custcares");
+// home.onload = function () 
+// {
+//     var lione = document.getElementById("lione");
+//     lione.style.backgroundColor = "white";
+//     lione.style.color = "blue";
+//     lione.style.width = "120%"
+//     lione.style.paddingLeft = "10%"
+//     lione.style.borderRadius = "7px"
+// } ;
+// shop.onload = function () 
+// {
+//     var litow = document.getElementById("litow");
+//     litow.style.backgroundColor = "white";
+//     litow.style.color = "blue";
+//     litow.style.width = "120%"
+//     litow.style.paddingLeft = "10%"
+//     litow.style.borderRadius = "7px"
+// } ;
+// sale.onload = function () 
+// {
+//     var lithree = document.getElementById("lithree");
+//     lithree.style.backgroundColor = "white";
+//     lithree.style.color = "blue";
+//     lithree.style.width = "120%"
+//     lithree.style.paddingLeft = "10%"
+//     lithree.style.borderRadius = "7px"
+// } ;
+// custcares.onload = function () 
+// {
+//     var lifour = document.getElementById("lifour");
+//     lifour.style.backgroundColor = "white";
+//     lifour.style.color = "blue";
+//     lifour.style.width = "120%"
+//     lifour.style.paddingLeft = "10%"
+//     lifour.style.borderRadius = "7px"
+// } ;
